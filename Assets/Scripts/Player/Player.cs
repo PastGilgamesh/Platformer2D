ca5cc19bd490a91d69ecca89f65e0ca73f173593
PastGilgamesh <80044981+PastGilgamesh@users.noreply.github.com>
2023-11-08 16:31:43 -0300

    Creating physics material

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,9 +6,11 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
 
     public float speed;
+
+    public float forceJump = 2;
 
     // Start is called before the first frame update
     void Start()
@@ -19,19 +21,50 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
-            
-            //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
-            
-            
-            //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
-
-        }
+        HandleJump();
+        HandleMoviment();
     }
+
+    private void HandleMoviment() 
+    {
+    if (Input.GetKey(KeyCode.LeftArrow))
+    {
+        myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
+        
+        //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
+    }
+    else if (Input.GetKey(KeyCode.RightArrow))
+    {
+    myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+            
+            
+    //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
+
+    }
+
+    if (myRigidbody.velocity.x > 0) 
+    {
+        myRigidbody.velocity -= friction;
+    }
+    else if (myRigidbody.velocity.x < 0)
+    {
+        myRigidbody.velocity += friction;
+    }
+
+    }
+
+private void HandleJump() 
+{
+
+ if (Input.GetKeyDown(KeyCode.Space))
+    {
+        myRigidbody.velocity = Vector2.up * forceJump;
+        
+    }
+
+
+}
+
+
+
 }
